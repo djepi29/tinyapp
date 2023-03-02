8a46fddecc08a59a8dc324c2b46f699d64e413e5
djepi29 <j3anpi3rr3g3org3s@email.com>
2023-03-02 17:27:43 -0500

    render a form

--- express_Server.js
+++ express_Server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
@@ -26,10 +27,15 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
+
 
 // app.get("/set", (req, res) => {
 //   const a = 1;
